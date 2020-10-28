7194d4dc67a95a4a4878cdaa9ef5b1af3c0f2eec
ManeeshSRautela <maneeshrautelamr@gmail.com>
2020-10-28 13:35:31 +0530

    First Git commit

---
+++ src/com/git/practise/TestGit.java
@@ -0,0 +1,12 @@
+package com.git.practise;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a simple eclipse Git practise project");
+		System.out.println("Thank you");
+
+	}
+
+}
